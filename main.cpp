a43a6a91747ba8788e87201b10d865df8d00f778
Antonio Itza <2024161195@ub.edu.bz>
2025-11-13 14:58:55 +0000

    Implemented Insert Function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Each Trie Node can store up to 26 letters (a-x)
@@ -15,7 +16,19 @@ TrieNode* getNode(){
      return node;
 }
 
+void insert(TrieNode* root, string word) {
+    TrieNode* curr = root;
+    for (char c : word) {
+        int index = c - 'a';
+        if (curr->children[index] == nullptr)
+            curr->children[index] = createNode();
+        curr = curr->children[index];
+    }
+    curr->isEndOfWord = true;
+}
+
 int main(){
+
 
 return 0;
 }
